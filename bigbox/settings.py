d4a646a501ab9c32185dde920302ece9cc125613
apsosa <patdeveloper.data@gmail.com>
2021-10-20 17:27:20 -0300

    set secret key

--- bigbox/settings.py
+++ bigbox/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 from my_secrets import secrets
 
-SECRET_KEY = secrets.SECRET_KEY
+SECRET_KEY =  "'django-insecure-8&(h_fz(jfgqd=dzdleldxg@-##e2)#78il-akh&8vd97cl((u'"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
