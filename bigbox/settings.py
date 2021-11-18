4248539c4ecd9bc6f3211cc9b6acbcf493e6bde1
apsosa <patdeveloper.data@gmail.com>
2021-11-18 13:27:46 -0300

    setup db

--- bigbox/settings.py
+++ bigbox/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'bigbox.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-""""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -119,7 +119,7 @@ DATABASES = {
         'PORT': '5432' 
     }
 }
-
+""""
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
