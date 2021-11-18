6160719be07f1bee4e31833aebf412d38ee7e061
apsosa <patdeveloper.data@gmail.com>
2021-11-18 13:24:31 -0300

    tutorial pat7 finished

--- bigbox/settings.py
+++ bigbox/settings.py
@@ -78,7 +78,7 @@ ROOT_URLCONF = 'bigbox.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'bigbox.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+""""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -108,7 +108,17 @@ DATABASES = {
         'PORT': '5432' 
     }
 }
-
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'big_box',
+        'USER': 'Clark',
+        'PASSWORD': 'kent',
+        'HOST': 'localhost',
+        'PORT': '5432' 
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
