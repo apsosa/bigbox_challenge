cca1e88f92cc8310c0ae92eb898f2d892bf47d26
apsosa <patdeveloper.data@gmail.com>
2021-11-19 15:50:41 -0300

    add quickstart app from djangoResr

--- bigbox/settings.py
+++ bigbox/settings.py
@@ -48,12 +48,13 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'django_extensions',
     'historical_data',
+    'quickstart'
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100,
+    'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
--- bigbox/urls.py
+++ bigbox/urls.py
@@ -18,8 +18,18 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 import debug_toolbar
+from rest_framework import routers
+from quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('core.api.urls')),
