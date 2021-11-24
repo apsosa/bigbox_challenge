f31ca08ce193c8e0e65ebdee96060ec3337b027c
apsosa <patdeveloper.data@gmail.com>
2021-11-24 10:59:07 -0300

    some fix

--- bigbox/urls.py
+++ bigbox/urls.py
@@ -22,7 +22,7 @@ from rest_framework import routers
 from quickstart import views
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
-from bigbox.schema import schema
+from .schema import schema
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -38,8 +38,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.api.urls')),
     path('/api-auth/', include('rest_framework.urls')),
-    #path("graphql", GraphQLView.as_view(graphiql=True)),
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
+    path('graphql', GraphQLView.as_view(graphiql=True, schema=schema)),
+    #path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
